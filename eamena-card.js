01e062b93ccf678b5538fd87707c7d0b99287a36
Ash Smith <ash.smith@soton.ac.uk>
2020-04-01 01:07:02 +0100

    Got basic add/delete functionality working thanks to Rob Gaston on Slack https://archesproject.slack.com/archives/GUKFHFK0A/p1584720580093900
    Still to do
    * Fix the data binding (ask Farallon how)
    * Fix the navigation tree 'focus stealing'

--- eamena-card.js
+++ eamena-card.js
@@ -2,8 +2,30 @@ define([
     'knockout',
     'viewmodels/card-component'
 ], function(ko, CardComponentViewModel) {
+
+    var viewModel = function(params) {
+        var self = this;
+        this.card = params.card;
+        this.form = params.form;
+        this.expanded = ko.observable(true);
+
+        this.saveValue = function(arg)
+        {
+            var index = arg();
+            var tiles = this.card.tiles()[0].cards[index].tiles();
+
+            var newtile = this.card.tiles()[0].cards[index].getNewTile();
+            var dataid = newtile.nodegroup_id;
+            newtile.data[dataid] = "Temporary value";
+            newtile.save();
+        };
+
+        params.configKeys = ['selectSource', 'selectSourceLayer'];
+
+        CardComponentViewModel.apply(this, [params]);
+    };
     return ko.components.register('eamena-card', {
-        viewModel: CardComponentViewModel,
+        viewModel: viewModel,
         template: {
             require: 'text!templates/views/components/card_components/eamena-card.htm'
         }
