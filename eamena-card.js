9782c0f12a35fd8a48eb5fc2a892050e8a120e68
Ash Smith <ash.smith@soton.ac.uk>
2020-04-08 16:13:38 +0100

    Data now goes nicely

--- eamena-card.js
+++ eamena-card.js
@@ -8,15 +8,19 @@ define([
         this.card = params.card;
         this.form = params.form;
         this.expanded = ko.observable(true);
+        this.data = ko.observableArray();
+
+	for(i = 0; i < 100; i++) { this.data.push(ko.observable('val ' + i)); }
 
         this.saveValue = function(arg)
         {
             var index = arg();
             var tiles = this.card.tiles()[0].cards[index].tiles();
 
+
             var newtile = this.card.tiles()[0].cards[index].getNewTile();
             var dataid = newtile.nodegroup_id;
-            newtile.data[dataid] = "Temporary value";
+            newtile.data[dataid] = this.data()[index]();
             newtile.save();
         };
 
